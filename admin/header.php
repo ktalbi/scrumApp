aea1d673e13c6e25329ed98c948981a8de382cad
2hill <jamil.rahjou@laposte.net>
2017-03-16 11:24:02 +0100

    clean script and style links

--- admin/header.php
+++ admin/header.php
@@ -3,8 +3,8 @@
 <html>
 <head>
     <meta charset="utf-8">
-<link rel="stylesheet" href="../css/dataTables.bootstrap.min.css"/>
-<link rel="stylesheet" href="../css/bootstrap.css" />
+<link rel="stylesheet" href="../bootstrap/css/dataTables.bootstrap.min.css"/>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css" />
 <link rel="stylesheet" href="../css/style.css" />
 <link rel="shortcut icon" href="favicon.ico"> <!-- fix the favicon error 404 -->
 
@@ -31,13 +31,13 @@ include("../config/boot.php");
 
   <!-- Links to  scripts -->
 
-<script src="../js/jquery-3.1.1.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/bootstrap-datetimepicker.js"></script>
-<script src="../js/jquery.dataTables.min.js"></script>
-<script src="../js/dataTables.bootstrap.min.js"></script>
-<script src="../js/highcharts.js"></script>
-<script src="../js/exporting.js"></script>
+<script src="../jquery/js/jquery-3.1.1.min.js"></script>
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+<script src="../bootstrap/js/bootstrap-datetimepicker.js"></script>
+<script src="../jquery/js/jquery.dataTables.min.js"></script>
+<script src="../bootstrap/js/dataTables.bootstrap.min.js"></script>
+<script src="../highcharts/js/highcharts.js"></script>
+<script src="../highcharts/js/exporting.js"></script>
 <script>
 //Set the active class for tab with page name
     $(document).ready(function(){
